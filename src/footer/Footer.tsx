2caf75414684c861ef20e8660f83aa32c3c69844
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-06-19 13:40:28 +0300

    '2nd part'

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import styleContainer from "../common/styles/Container.module.css";
 import s from "./Footer.module.scss";
+import GitHub from '../assets/image/GitHub.png'
+import telegram from '../assets/image/telegram.png'
+import gmail from '../assets/image/gmail.png'
+
 
 export const Footer = () => {
     return (
-        <div className={s.footer}>
+        <div className={s.footerBlock}>
             <div className={`${styleContainer.container} ${s.footerContainer}`}>
-                <h2 className={s.title}>Ekaterina Yurchyk</h2>
-                <div className={s.socialIcons}>
+                <h2 >Ekaterina Yurchyk</h2>
+                <div className={s.footerContacts}>
+                    <a href={'https://github.com/EkaterinaYurchyk2022'} target={'_blank'}>
+                        <img src={GitHub} className={s.footerItem}/>
+                    </a>
+                    <a href={'https://t.me/EkaterinaYurchyk'} target={'_blank'}>
+                        <img src={telegram} className={s.footerItem}/>
+                    </a>
+                    <a href={'mailto:ekaterina.yurchyk.s@gmail.com'}>
+                        <img src={gmail} className={s.footerItem}/>
+                    </a>
+              {/*      <div className={s.socialIcon}></div>
                     <div className={s.socialIcon}></div>
                     <div className={s.socialIcon}></div>
                     <div className={s.socialIcon}></div>
-                    <div className={s.socialIcon}></div>
-
+*/}
                 </div>
-                <span className={s.copyright}>© 2022 All Rights Reserved</span>
+                <div className={s.footerEnd}>© 2022 All Rights Reserved</div>
             </div>
 
         </div>
